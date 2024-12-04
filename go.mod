364540ea984b721b92e6083042ba5bebd70d06c6
marcuscastelo <marcuscastelo@usp.br>
2024-12-04 11:00:39 -0300

    no perms

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/pion/turn/v4
+
+go 1.20
+
+require (
+	github.com/pion/logging v0.2.2
+	github.com/pion/randutil v0.1.0
+	github.com/pion/stun/v3 v3.0.0
+	github.com/pion/transport/v3 v3.0.7
+	github.com/stretchr/testify v1.9.0
+	golang.org/x/sys v0.26.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pion/dtls/v3 v3.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/wlynxg/anet v0.0.3 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/server/stun.go
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+package server
+
+import (
+	"github.com/pion/stun/v3"
+	"github.com/pion/turn/v4/internal/ipnet"
+)
+
+func handleBindingRequest(r Request, m *stun.Message) error {
+	r.Log.Debugf("(STUN) Received BindingRequest from %s", r.SrcAddr)
+
+	ip, port, err := ipnet.AddrIPPort(r.SrcAddr)
+	if err != nil {
+		r.Log.Errorf("Failed to extract IP and Port from %s: %v", r.SrcAddr, err)
+		return err
+	}
+
+	attrs := buildMsg(m.TransactionID, stun.BindingSuccess, &stun.XORMappedAddress{
+		IP:   ip,
+		Port: port,
+	}, stun.Fingerprint)
+
+	r.Log.Debugf("(STUN) Responding to BindingRequest from %s", r.SrcAddr)
+	return buildAndSend(r.Conn, r.SrcAddr, attrs...)
+}
